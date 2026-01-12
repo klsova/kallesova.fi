2df88b97532db1f962001d53ad6456771cf8f756
klsova <kane.sova@gmail.com>
2026-01-12 02:15:59 +0200

    added animation for page text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,20 @@
 
 import Link from "next/link";
 import { useTheme } from "next-themes";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { FaSun, FaMoon, FaFolder } from "react-icons/fa";
 
 export default function Navbar() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const pathname = usePathname();
 
   useEffect(() => {
     setMounted(true);
   }, []);
+
+  const pathSuffix = pathname === "/" ? "" : pathname;
 
   return (
     <nav>
@@ -20,18 +24,21 @@ export default function Navbar() {
             <span className="prompt-user">kallesova@mysite</span>
             <span className="prompt-symbol">:</span>
             <span className="prompt-path">~</span>
+            
+            <span className="path-slider">
+                <span className="path-content">{pathSuffix}</span>
+            </span>
+            
             <span className="prompt-symbol">$</span>
-            <span className="cursor">_</span>
+            <span className="cursor">_</span> 
         </Link>
       </div>
 
       <div style={{ display: "flex", gap: "20px" }}>
-        {/* Works Button */}
         <Link href="/works" className="nav-btn">
-          <FaFolder style={{ fontSize: '0.9rem' }}/> Works
+          <FaFolder style={{ fontSize: '0.8rem' }}/> Works
         </Link>
 
-        {/* Theme Toggle Button */}
         <button
           className="nav-btn"
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
